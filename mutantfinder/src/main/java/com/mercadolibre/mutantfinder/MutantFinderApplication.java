2fb981fffde9ded1f4abdc91e8adf61266f7d58b
Javier Gonzalez <javiermartingonzalez@gmail.com>
2018-12-05 22:37:48 -0300

    First commit

---
+++ mutantfinder/src/main/java/com/mercadolibre/mutantfinder/MutantFinderApplication.java
@@ -0,0 +1,12 @@
+package com.mercadolibre.mutantfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantFinderApplication.class, args);
+	}
+}
