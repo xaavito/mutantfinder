4613b431677ad2f2c516fe654a4e4770c0203403
Javier Gonzalez <javiermartingonzalez@gmail.com>
2018-12-07 11:41:04 -0300

    Infraestructura andando, recibe post y obtiene gets pegandole a un MongoDB embeded, mejor que la alternativa de la Otra BD que habai que instalarla

--- mutantfinder/src/main/java/com/mercadolibre/mutantfinder/entity/Adn.java
+++ mutantfinder/src/main/java/com/mercadolibre/mutantfinder/entity/Adn.java
@@ -3,14 +3,20 @@ package com.mercadolibre.mutantfinder.entity;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
 @Document(collection = "adn")
+@Data
+@NoArgsConstructor
 public class Adn {
-	private @Id Long id;
+	private @Id  String id;
 	private String[] dna = new String[6];
 	private int isMutant;
 
-	Adn(String[] sec) {
-		this.dna = sec;
+	public Adn(String[] dna) {
+		this.dna = dna;
 		this.isMutant = 0;
 	}
 
