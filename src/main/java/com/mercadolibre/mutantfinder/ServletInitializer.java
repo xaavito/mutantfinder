bbf2a3dafc55340b245197d70a04fa2637b6f102
Javier Gonzalez <javiermartingonzalez@gmail.com>
2018-12-11 00:40:05 -0300

    Mas Cambios, se quitan toda la gloria del google app engine

--- src/main/java/com/mercadolibre/mutantfinder/ServletInitializer.java
+++ src/main/java/com/mercadolibre/mutantfinder/ServletInitializer.java
@@ -2,10 +2,16 @@ package com.mercadolibre.mutantfinder;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
+/**
+ * Solo para subir al google app engine
+ * @author Javier Gonzalez
+ *
+ */
+public class ServletInitializer /*extends SpringBootServletInitializer */{
+	/*
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(MutantFinderApplication.class);
 	}
+	*/
 }
