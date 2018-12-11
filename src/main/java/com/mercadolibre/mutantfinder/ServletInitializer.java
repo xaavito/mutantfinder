fa0819ca808a760933da83e092fd14bd96a283d1
Javier Gonzalez <javiermartingonzalez@gmail.com>
2018-12-10 23:02:37 -0300

    Mas cambios para intentar deployar la app

---
+++ src/main/java/com/mercadolibre/mutantfinder/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.mercadolibre.mutantfinder;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MutantFinderApplication.class);
+	}
+}
